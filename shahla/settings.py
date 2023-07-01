761dd65f57800dacb5ae21afda754b3d14904882
pouya <you@example.com>
2023-07-01 12:00:08 +0330

    fix debug

--- shahla/settings.py
+++ shahla/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-g3fv-yqw8jmcb=bnhhpf(q#nfp+(pv3x6x5nbqkt8tb99k_5xc'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
