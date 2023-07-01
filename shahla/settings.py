b4c719fd541004329779b0c84beaaa02918af7cb
pouya <you@example.com>
2023-07-01 20:24:30 +0330

    fix farsi numbers

--- shahla/settings.py
+++ shahla/settings.py
@@ -65,7 +65,12 @@ TEMPLATES = [
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
             ],
+            'libraries': {
+                'custom_templatetag': 'apps.menus.templatetags.farsi',
+
+            }
         },
+
     },
 ]
 
