f40b8b3edc49237707cde841ccb74d51c5485f34
pouya <you@example.com>
2023-07-01 12:20:24 +0330

    fix user admin

--- apps/users/admin.py
+++ apps/users/admin.py
@@ -3,11 +3,12 @@ from django.contrib.auth import get_user_model
 from django.contrib.auth.admin import GroupAdmin
 from django.contrib.auth.forms import UserChangeForm, UserCreationForm
 from django.contrib.auth.models import Group
+from django.contrib.auth import admin as auth_admin
 
 User = get_user_model()
 
 
-class UserAdmin(admin.ModelAdmin):
+class UserAdmin(auth_admin.UserAdmin):
     list_display = ['name', 'email']
     form = UserChangeForm
     add_form = UserCreationForm
