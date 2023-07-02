a3e8652142468d0a8840ffec7ee0f79c1e6afd0b
pouya <you@example.com>
2023-07-02 11:41:16 +0330

    multipe fixes

--- apps/menus/templatetags/farsi.py
+++ apps/menus/templatetags/farsi.py
@@ -1,4 +1,5 @@
 from django import template
+from django.utils import timezone
 
 register = template.Library()
 
@@ -8,3 +9,9 @@ def persian_int(english_int):
     devanagari_nums = ('۰', '۱', '۲', '۳', '۴', '۵', '۶', '۷', '۸', '۹')
     number = str(english_int)
     return ''.join(devanagari_nums[int(digit)] for digit in number)
+
+
+@register.filter(name='check_time')
+def check_time(hour):
+    now = timezone.now().hour
+    return hour > now
